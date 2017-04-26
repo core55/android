4823e7c40a513ec94708a77de7b1ea802e1b46cd
Patrick Richer St-Onge <prso@kth.se>
2017-04-26 13:14:14 +0200

    Add handling of app links
    Linked to https://patrick-rst.github.io

--- app/src/main/java/io/github/core55/joinup/MainActivity.java
+++ app/src/main/java/io/github/core55/joinup/MainActivity.java
@@ -1,5 +1,7 @@
 package io.github.core55.joinup;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Handle app links
+        Intent appLinkIntent = getIntent();
+        String appLinkAction = appLinkIntent.getAction();
+        Uri appLinkData = appLinkIntent.getData();
     }
 }
