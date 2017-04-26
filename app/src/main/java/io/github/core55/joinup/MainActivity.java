ead8b4b0364337c22dccaff63b3dc868b50ae424
juanluisrto <juanluis.rto@gmail.com>
2017-04-26 13:06:43 +0200

    App with a mapview

--- app/src/main/java/io/github/core55/joinup/MainActivity.java
+++ app/src/main/java/io/github/core55/joinup/MainActivity.java
@@ -1,7 +1,9 @@
 package io.github.core55.joinup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goToMap(View v){
+        Intent intent = new Intent(MainActivity.this, MapsActivity.class);
+        startActivity(intent);
     }
 }
