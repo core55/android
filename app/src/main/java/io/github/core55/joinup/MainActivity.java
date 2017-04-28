831d4759a19b8a4ba108954721e7b1fc0bec6950
Hussam Hassanein <hussamh@kth.se>
2017-04-28 11:15:28 +0200

    Add share button with copy function

--- app/src/main/java/io/github/core55/joinup/MainActivity.java
+++ app/src/main/java/io/github/core55/joinup/MainActivity.java
@@ -1,13 +1,49 @@
 package io.github.core55.joinup;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageButton mShowDialog = (ImageButton) findViewById(R.id.imageButton);
+        mShowDialog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                AlertDialog.Builder mBuilder = new AlertDialog.Builder(MainActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.dialog_share, null);
+                mBuilder.setView(mView);
+                final AlertDialog dialog = mBuilder.create();
+                dialog.show();
+                dialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+
+            }
+
+        });
+
     }
+
+    //copyToclipboard
+    public void copyToCliboard(View v) {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("label", "it actually works!!!");
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(this,"Link is copied!", Toast.LENGTH_SHORT).show();
+    }
+
+
 }
