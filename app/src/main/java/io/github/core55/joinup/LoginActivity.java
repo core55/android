28944720eb7248651374d61a7a1593819c0c14b7
Simone Stefani <s.stefani95@gmail.com>
2017-05-02 16:17:59 +0200

    Add basic login with username and password

--- app/src/main/java/io/github/core55/joinup/LoginActivity.java
+++ app/src/main/java/io/github/core55/joinup/LoginActivity.java
@@ -6,9 +6,22 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.util.HashMap;
 
 public class LoginActivity extends Activity {
 
@@ -27,11 +40,43 @@ public class LoginActivity extends Activity {
         startActivity(i);
 
     }
-    protected void emailLogin (){
+
+
+    protected void emailLogin(View v){
         EditText emailEditText = (EditText) findViewById(R.id.etUsername);
         String email =  emailEditText.getText().toString();
 
         EditText passwordEditText = (EditText) findViewById(R.id.etPassword);
-        String password =  emailEditText.getText().toString();
+        String password =  passwordEditText.getText().toString();
+
+        loginBackend(email, password);
+
+    }
+
+    private void loginBackend(String username, String password) {
+        RequestQueue queue = Volley.newRequestQueue(this);
+        final String URL = "http://130.229.144.78:8080/api/login";
+
+        HashMap<String, String> params = new HashMap<String, String>();
+        params.put("username", username);
+        params.put("password", password);
+
+        JsonObjectRequest request_json = new JsonObjectRequest(Request.Method.POST,
+                URL,
+                new JSONObject(params),
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        Log.d("PEW", response.toString());
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        VolleyLog.e("Error: ", error.getMessage());
+                    }
+                });
+
+        queue.add(request_json);
     }
 }
