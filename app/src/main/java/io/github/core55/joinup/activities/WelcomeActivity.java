aa303b5714c8686ef7e86ca7ef157ef70a0803de
Patrick Richer St-Onge <prso@kth.se>
2017-05-09 15:06:56 +0200

    Fix bug causing release build crash

--- app/src/main/java/io/github/core55/joinup/activities/WelcomeActivity.java
+++ app/src/main/java/io/github/core55/joinup/activities/WelcomeActivity.java
@@ -10,6 +10,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import io.github.core55.joinup.R;
 import io.github.core55.joinup.activities.CreateActivity;
@@ -29,6 +30,23 @@ public class WelcomeActivity extends Activity {
             Intent i = new Intent(this, CreateActivity.class);
             startActivity(i);
         }
+
+        Button btn_login = (Button) findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createMeetup(v);
+            }
+        });
+
+        Button btn_login_welcome = (Button) findViewById(R.id.btn_login_welcome);
+        btn_login_welcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginActivity(v);
+            }
+        });
+
     }
 
     protected void createMeetup(View v) {
