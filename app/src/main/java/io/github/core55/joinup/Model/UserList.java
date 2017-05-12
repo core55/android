ab6c25f97e0dadb11569911062559c77bbdf24d8
Simone Stefani <s.stefani95@gmail.com>
2017-05-12 10:19:13 +0200

    Implement Gson request to fetch users list

---
+++ app/src/main/java/io/github/core55/joinup/Model/UserList.java
@@ -0,0 +1,47 @@
+package io.github.core55.joinup.Model;
+
+import java.util.Collection;
+import java.util.List;
+
+import io.github.core55.joinup.Entity.User;
+
+public class UserList {
+
+    private _embedded _embedded;
+
+    public UserList() {
+    }
+
+    public UserList(_embedded _embedded) {
+        this();
+        this._embedded = _embedded;
+    }
+
+    public List<User> getUsers() {
+        return _embedded.getUsers();
+    }
+
+    public void setUsers(List<User> users) {
+        this._embedded.setUsers(users);
+    }
+
+    public class _embedded {
+        private List<User> users;
+
+        public _embedded() {
+        }
+
+        public _embedded(List<User> users) {
+            this();
+            this.users = users;
+        }
+
+        public List<User> getUsers() {
+            return users;
+        }
+
+        public void setUsers(List<User> users) {
+            this.users = users;
+        }
+    }
+}
